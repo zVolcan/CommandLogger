3253316f5e26013fb6eb3dc2e3d34d3a777e9d8e
volcqnn <emir49499141@gmail.com>
2026-05-22 17:04:55 -0300

    fix: add shadow plugin for Gradle and configure shadowJar settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java-library")
     id("xyz.jpenilla.run-paper") version "3.0.2"
+    id("com.gradleup.shadow") version "9.4.0"
 }
 
 repositories {
@@ -35,5 +36,9 @@ tasks {
         filesMatching("paper-plugin.yml") {
             expand(props)
         }
+    }
+
+    shadowJar {
+        archiveClassifier.set("")
     }
 }
