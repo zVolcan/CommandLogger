c004217cc6a1a6a8c4569e8f835c1c738ae9eb6c
volcqnn <emir49499141@gmail.com>
2026-05-27 18:19:13 -0300

    feat: update Paper API and Minecraft version, add LuckPermsManager class

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,12 +14,13 @@ dependencies {
     compileOnly("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
 
-    compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly("net.luckperms:api:5.4")
     implementation("com.github.eduardomcb:discord-webhook:1.0.0")
 }
 
 java {
-    toolchain.languageVersion = JavaLanguageVersion.of(17)
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
 }
 
 tasks {
@@ -27,7 +28,7 @@ tasks {
         // Configure the Minecraft version for our task.
         // This is the only required configuration besides applying the plugin.
         // Your plugin's jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("1.18.2")
+        minecraftVersion("1.21.11")
         jvmArgs("-Xms2G", "-Xmx2G")
     }
 
---
+++ src/main/java/xyz/zenith/commandLogger/luckperms/LuckPermsManager.java
@@ -0,0 +1,50 @@
+package xyz.zenith.commandLogger.luckperms;
+
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.event.node.NodeAddEvent;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.Node;
+import net.luckperms.api.node.types.InheritanceNode;
+import xyz.zenith.commandLogger.CommandLogger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class LuckPermsManager {
+
+    private final Map<String, List<String>> cache = new HashMap<>();
+    private CommandLogger plugin;
+    private LuckPerms luckPerms;
+
+    public LuckPermsManager(final CommandLogger plugin) {
+        if (plugin.getServer().getPluginManager().getPlugin("LuckPerms") == null) {
+            plugin.getLogger().warning("LuckPerms is not installed! CommandLogger will not be able to fetch player prefixes and suffixes.");
+            return;
+        }
+
+        this.plugin = plugin;
+        this.luckPerms = plugin.getServer().getServicesManager().load(LuckPerms.class);
+    }
+
+    public List<String> getPlayerCache(String playerName) {
+        return cache.getOrDefault(playerName, new ArrayList<>());
+    }
+
+    public void registerEventLog() {
+        luckPerms.getEventBus().subscribe(plugin,
+                NodeAddEvent.class
+        , event -> {
+            User user = (User) event.getTarget();
+            Node node = event.getNode();
+
+            if (node instanceof InheritanceNode nodoGroup) {
+                final String groupName = nodoGroup.getGroupName();
+                List<String> groups = getPlayerCache(user.getUsername());
+                groups.add(groupName);
+                cache.put(user.getUsername(), groups);
+            }
+        }).close();
+    }
+}
